99bf89b68af0b33ddc950c5b8eb0e77ffdfbd00d
Fornalmeister <64432550+Fornalmeister@users.noreply.github.com>
2024-05-17 13:31:53 +0200

    Add project files.

---
+++ Builder/Progam.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            var builder = new InvoiceBuilder();
+            
+            Invoice invoice = builder.SetDate(DateTime.Now)
+                .SetInvoiceNumber("Invoice-01")
+                .SetVendor("Vendor")
+                .SetVendee("Vendee")
+                .SetLineItems(new List<string>() {"Line item1", "Line item2"})
+                .Build();
+
+            Console.WriteLine(invoice.Vendee);
+        }
+    }
+}
